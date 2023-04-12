c06f51953febaec086a3fd3025e39665774294f7
btom <tom.benat@ynov.com>
2023-04-12 20:10:13 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("Page/cv.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+	fs := http.FileServer(http.Dir("Static/"))
+	http.Handle("/Static/", http.StripPrefix("/Static/", fs))
+	fmt.Println("http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
