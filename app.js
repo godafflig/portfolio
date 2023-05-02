f124245d2e26e6063c6672763f08a405bbc2f7b0
btom <tom.benat@ynov.com>
2023-05-02 09:43:27 +0200

    page index

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 let open = false
 
 document.getElementById("burger-button").onclick = () => {
+    const menu = document.getElementById("menu")
     const burgerContent = document.getElementById("burger-content")
     open = !open
-    open ? burgerContent.classList.add("burger-open") : burgerContent.classList.remove("burger-open");
+    open ? burgerContent.classList.add("burger-open") && menu.classList.add("menu-open") : burgerContent.classList.remove("burger-open") && menu.classList.remove("menu-open");
 }
